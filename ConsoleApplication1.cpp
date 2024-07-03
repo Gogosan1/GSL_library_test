50ac3d55026780a2071b1d9d780d3c5d8b863d5c
Pavel Zelenev <pakanzelenev@mail.ru>
2024-07-03 23:40:00 +0700

    9 task done 2methods

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -9,10 +9,13 @@
 int func(double t, const double y[], double f[], void* params) {
     (void)(t); // Используется для подавления предупреждений о неиспользуемых параметрах
     // double lambda = *(double*)params;
-    f[0] = -y[0] + 2;
-    f[1] = -10*y[1] + 0.1*y[0]*y[0];
-    f[2] = -40*y[2] + 0.4*(y[0]*y[0] + y[1]*y[1]);
-    f[3] = -100*y[3] + y[0]*y[0] + y[1]*y[1] * y[2]*y[2];
+    f[0] = -y[0];
+    f[1] = y[0] - 2*y[1];
+    for (int i = 3; i <= 9; i++)
+    {
+        f[i - 1] = (i - 1)*y[i - 1] - i*y[i];
+    }
+    f[9] = 9*y[8];
     return GSL_SUCCESS;
 }
 
@@ -41,15 +44,15 @@ int main()
 
 void solve_and_write_on_file(const char *file_name, double lambda, const gsl_odeiv2_step_type * T)
 {
-    double y[4] = { 1.0, 1.0, 1.0, 1.0}; // начальные условия
+    double y[10] = { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 , 0.0, 0.0, 0.0 }; // начальные условия
 
     double t0 = 0.0, tk = 20;  // начальная и конечная точки интегрирования
-    double hstart = 1e-2; // величина шага
+    double hstart = 5e-2; // величина шага
     
     
     double minh = 1e-10, maxh = 0.0; // границы точности, левая и правая
 
-    gsl_odeiv2_system sys = { func, NULL, 4, NULL};
+    gsl_odeiv2_system sys = { func, NULL, 10, NULL};
     gsl_odeiv2_driver* d = gsl_odeiv2_driver_alloc_y_new(&sys, T, hstart, minh, maxh);
     FILE* file;
     errno_t err_rk4;
@@ -60,7 +63,7 @@ void solve_and_write_on_file(const char *file_name, double lambda, const gsl_ode
         return;
     }
 
-    fprintf(file, "t y1 y2 y3 y4\n");
+    fprintf(file, "t y1 y2 y3 y4 y5 y6 y7 y8 y9 y10\n");
 
     // Цикл интегрирования с фиксированным шагом
     for (double ti = t0; ti <= tk; ti += hstart) {
@@ -70,7 +73,7 @@ void solve_and_write_on_file(const char *file_name, double lambda, const gsl_ode
             fprintf(stderr, "Ошибка при интегрировании: %s\n", gsl_strerror(status_rk4));
             break;
         }
-        fprintf(file, "%.10f %.10f %.10f %.10f %.10f\n", ti, y[0], y[1], y[2], y[3]);
+        fprintf(file, "%.10f %.10f %.10f %.10f %.10f %.10f %.10f %.10f %.10f %.10f %.10f\n", ti, y[0], y[1], y[2], y[3], y[4], y[5], y[6], y[7], y[8], y[9]);
     }
 
 
